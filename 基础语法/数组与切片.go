f1b171bb63ab7ffd3ea1abe55b16b92026a508a7
caozhaoyuan <18801435616@163.com>
2023-06-30 18:10:53 +0800

    go日常学习碎片整理-数组与切片

--- 基础语法/数组与切片.go
+++ 基础语法/数组与切片.go
@@ -8,9 +8,10 @@ func main() {
 	arr1 := [...]int{1, 2, 3, 4} //不指定长度
 	fmt.Println(arr, arr1)
 	//切片
-	slice1 := make([]int, 3) //指定长度
-	slice2 := []int{3, 1, 2}
+	slice1 := make([]int, 3)    //指定长度
+	slice2 := []int{3, 1, 2}    //不指定长度
 	slice3 := make([]int, 3, 5) //指定长度(len),容量(cap)
-	fmt.Println(slice1, slice2, slice3)
+	slice4 := arr[:]            //从数组中初始化
+	fmt.Println(slice1, slice2, slice3, slice4)
 
 }
